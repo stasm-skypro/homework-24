f416af9c27e87c5a43558a0437a0d55fb87bff9d
Stanislav Mayatsky <stasm226@gmail.com>
2024-12-28 21:23:08 +0500

    implemented adding product throw form into the database

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from catalog.models import Contact
+from django.forms import ClearableFileInput
+
+from catalog.models import Contact, Product
 
 
 class ContactForm(forms.ModelForm):
@@ -10,4 +12,19 @@ class ContactForm(forms.ModelForm):
             "first_name": forms.TextInput(attrs={"class": "form-control", "placeholder": "Ваше имя"}),
             "phone": forms.TextInput(attrs={"class": "form-control", "placeholder": "Контактный телефон"}),
             "message": forms.Textarea(attrs={"class": "form-control", "placeholder": "Сообщение"}),
+        }
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ["product", "description", "image", "category", "price", "created_at", "changed_at"]
+        widgets = {
+            "product": forms.TextInput(attrs={"class": "form-control", "placeholder": "Наименование продукта"}),
+            "description": forms.Textarea(attrs={"class": "form-control", "placeholder": "Описание"}),
+            "image": ClearableFileInput(attrs={"class": "form-control"}),
+            "category": forms.TextInput(attrs={"class": "form-control", "placeholder": "Категория"}),
+            "price": forms.TextInput(attrs={"class": "form-control", "placeholder": "Цена"}),
+            "created_at": forms.TextInput(attrs={"class": "form-control", "placeholder": "Дата производства"}),
+            "changed_at": forms.TextInput(attrs={"class": "form-control", "placeholder": "Дата упаковки"}),
         }
--- catalog/views.py
+++ catalog/views.py
@@ -4,10 +4,11 @@
 
 from django.shortcuts import render
 from catalog.models import Product
-from catalog.forms import ContactForm
+from catalog.forms import ContactForm, ProductForm
 
 
-def home_page(request):
+
+def home_view(request):
     """
     Определяет отображение страницы home.html.
     """
@@ -15,7 +16,7 @@ def home_page(request):
     return render(request,"home.html", context={"products_list": products_list})
 
 
-def contacts_page(request):
+def contacts_view(request):
     """
     Определяет отображение страницы contacts.html.
     """
@@ -29,9 +30,23 @@ def contacts_page(request):
     return render(request, "contacts.html", {"form": form})
 
 
-def product_details_page(request, product_id: int = 1):
+def product_details_view(request, product_id: int = 1):
     """
     Определяет отображение детализации (характеристик) продукта.
     """
     product = Product.objects.get(id=product_id)
     return render(request, "product_details.html", context={"product": product})
+
+
+def add_product_view(request):
+    """
+    Определяет отображение добавления продукта.
+    """
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()  # Сохраняем данные в таблицу catalog_products
+            return render(request, "success.html")
+    else:
+        form = ProductForm()
+    return render(request, "add_product.html", {"form": form})
